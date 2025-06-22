c1b3077e68888f72541a739d4e31f8f59b381019
varshith19-ctrl <varshithkaitoju@gmail.com>
2025-06-22 14:20:49 +0530

    fix: set secure cookie flag to true for enhanced security

--- backend/src/controllers/JournalRoutes.controller.js
+++ backend/src/controllers/JournalRoutes.controller.js
@@ -58,7 +58,7 @@ export const loginUser = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production", // 👈 true in production
+        secure: true, // 👈 true in production
         sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       })
